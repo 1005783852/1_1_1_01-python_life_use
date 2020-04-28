76e8b97b24ecbafc16f929f3158db29c1fb34d42
1005783852 <48930677+1005783852@users.noreply.github.com>
2020-04-28 13:27:54 +0800

    Add File
    #功能：自动打印一年用日历。

---
+++ Calendar.py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+import calendar
+ #功能：自动打印整年日历
+cal2=calendar.calendar(2019,w=2,l=1,c=2)
+print(cal2)
+
+
+#cal = calendar.month(2019, 1)
+#print("以下输出2019年1月份的日历:")
+#print(cal)
